95bdcdd600efb9f9dc88219a1ae7fe3ff566d0d7
Paul OGGERO <paul.oggero@itgrate.com>
2023-08-28 18:42:29 +0700

    Initial Commit

---
+++ MovieList/MovieListApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieListApp.swift
+//  MovieList
+//
+//  Created by Paul Oggero on 28/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct MovieListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
